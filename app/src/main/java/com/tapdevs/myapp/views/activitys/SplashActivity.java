6cb2c29e4cc72702776121f19c27cc484d47f4bb
JanshairKhan <janshair.khan@gmail.com>
2017-01-30 14:36:35 +0000

    Added Splash Activity

---
+++ app/src/main/java/com/tapdevs/myapp/views/activitys/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.tapdevs.myapp.views.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tapdevs.myapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
