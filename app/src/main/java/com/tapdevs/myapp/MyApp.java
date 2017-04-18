a345762729da0c02aa780e100fe68dca7b238005
Jan Shair <jan.shair@skybettingandgaming.com>
2017-04-18 17:12:22 +0100

    Added Leak Canary

--- app/src/main/java/com/tapdevs/myapp/MyApp.java
+++ app/src/main/java/com/tapdevs/myapp/MyApp.java
@@ -3,6 +3,7 @@ package com.tapdevs.myapp;
 import android.app.Application;
 import android.content.Context;
 
+import com.squareup.leakcanary.LeakCanary;
 import com.tapdevs.myapp.injections.component.DaggerNetComponent;
 import com.tapdevs.myapp.injections.component.NetComponent;
 import com.tapdevs.myapp.injections.modules.AppModule;
@@ -27,6 +28,12 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         mNetComponent = DaggerNetComponent.builder()
                 // list of modules that are part of this component need to be created here too
                 .appModule(new AppModule(this)) // This also corresponds to the name of your module: %component_name%Module
